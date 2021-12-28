062e998a1f920d30e7c27eb282d02c65e8e1addd
Wapic <fallon955@gmail.com>
2021-12-28 14:33:55 +0100

    aurbuilder improvments and disable mouse accel on i3 start

--- scripts/aurbuild.py
+++ scripts/aurbuild.py
@@ -3,33 +3,37 @@ import sys
 import requests
 import subprocess
 import os
+import pathlib
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        print("not enough arguments")
+        print("Not enough arguments.\nusage: aur <install/search> <package-name>")
         quit()
 
     url = f"https://aur.archlinux.org/rpc/?v=5&type=search&arg={str(sys.argv[2])}"
     x = requests.get(url)
     result = x.json()["results"]
     
-    if len(result) >= 1:
-        name = result[0]["Name"]
-    else:
-        print("no results found")
+    if len(result) < 1 :
+        print("No matching results")
         quit()
-        
+  
     if str(sys.argv[1]).lower() == "install": 
-    
+        name = result[0]["Name"]
         cloneURL = f"https://aur.archlinux.org/{name}.git"
         print(f"found: {cloneURL}")
         
         if input("continue? [y/n] ") == "y":
             dir = f"{os.environ['HOME']}/builds/{name}/"
-        
-            subprocess.run(["git", "clone", cloneURL, dir])
-            subprocess.run(f"(cd {dir} && makepkg -si)", shell=True)
+            
+            try:
+                if pathlib.Path(dir).exists: raise Exception()
+                subprocess.run(["git", "clone", cloneURL, dir])
+            except:
+                print("Folder already exists, attempting to build")
+            finally:
+                subprocess.run(f"(cd {dir} && makepkg -si)", shell=True)
 
     elif str(sys.argv[1]).lower() == "search":
         for entries in result:
