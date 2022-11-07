0c5353265e31f40eb57f7e706cc925677d9d9aee
Wapic <fallon955@gmail.com>
2022-11-07 04:20:53 +0100

    calc code clean up

--- scripts/calc.py
+++ scripts/calc.py
@@ -3,13 +3,16 @@ import sys
 import re
  
 def getZeros(str):
-    size = 0
+    size = 0 
     if str.endswith("k"):
         size = 3
-    if str.endswith("m"):
+    elif str.endswith("m"):
         size = 6
-    if str.endswith("b"):
+    elif str.endswith("b"):
         size = 9
+
+    if not str.startswith("."):
+        size = size + len(str) - 1
 
     str = re.sub("[.mbk]", "", str)
     str = str.ljust(size, "0")
@@ -20,25 +23,18 @@ def formatValues(user_input):
 
     user_input = user_input.lower() # make input lowercase
 
-    stringList = re.findall("(\.\d+[mbk])", user_input) # add all matching strings into a list (matching examples: 1.2m, 5.23b, 92.1k, etc) 
+    stringList = re.findall("(\.*\d+[mbk])", user_input) # add all matching strings into a list 
     for string in stringList:
-        user_input = re.sub("(\.\d+[mbk])", getZeros(string), user_input, 1) # replace old value with new formatted value
-
-    stringList = re.findall("[mbk]", user_input)
-    for string in stringList:
-        user_input = re.sub("[mbk]", getZeros(string), user_input, 1)
+        user_input = re.sub("(\.*\d+[mbk])", getZeros(string), user_input, 1) # replace old value with new formatted value
 
     return user_input
 
 def main():
     user_input = sys.argv[1] if len(sys.argv) > 1 else None
     if user_input:
-        try:
-            user_input = formatValues(user_input)
-            result = eval(user_input)
-            print(f"{result:,}")
-        except Exception:
-            return
+        user_input = formatValues(user_input)
+        result = eval(user_input)
+        print(f"{result:,}")
 
 if __name__ == "__main__":
     main()
