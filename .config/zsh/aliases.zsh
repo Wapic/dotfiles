d54e49d4be0507cbca509567d635b9f9749680eb
Wapic <mail@linuslundqvist.se>
2024-10-28 08:32:26 +0100

    laptop setup

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,10 +1,8 @@
 # QOL
 alias sudo='sudo '
-alias plz='sudo !!'
 alias cp='cp -i'                         # confirm before overwriting something
 alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
-alias np='nano -w PKGBUILD'
 alias more=less
 alias vim='nvim'
 alias cmake='make -j $(nproc)'
@@ -16,7 +14,7 @@ alias aur='/home/wapic/scripts/aurbuild.py'
 alias yt-dl='/home/wapic/scripts/yt-dl.py'
 
 # CHANGE DIR
-alias wpc='cd /home/wapic/.local/share/PrismLauncher/instances/1.8.9/.minecraft/config/ChatTriggers/modules/'
+alias wpc='cd /home/wapic/.local/share/PrismLauncher/instances/SKYBLOCK_OLD/minecraft/config/ChatTriggers/modules/'
 alias nvssd='cd /mnt/nvssd/'
 alias hdd="cd /mnt/hdd/"
 alias ssd="cd /mnt/ssd/"
