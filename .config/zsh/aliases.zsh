1d1f533b735be3b5beb2d04e484b28281adac086
Wapic <17051068+Wapic@users.noreply.github.com>
2024-11-22 14:28:49 +0100

    add mv & vi alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,14 +2,12 @@
 alias sudo='sudo '
 alias plz='sudo !!'
 alias cp='cp -i'                         # confirm before overwriting something
-alias df='df -h'                          # human-readable sizes
-alias free='free -m'                      # show sizes in MB
-alias np='nano -w PKGBUILD'
-alias more=less
 alias vim='nvim'
+alias vi='nvim'
 alias cmake='make -j $(nproc)'
 alias ls='ls --group-directories-first -X --color=always'
 alias xprop="hyprprop"
+alias mv="mv -v"    #show what is being done 
 
 # SCRIPTS
 alias aur='/home/wapic/scripts/aurbuild.py'
