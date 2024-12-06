3fcb607c2279b0bd044c5a5c6454c1860756d7d1
Wapic <17051068+Wapic@users.noreply.github.com>
2024-12-06 16:10:01 +0100

    replace python script with shell script

---
+++ scripts/yt-dl.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+url=$1
+file_format=$2
+args="-f mp4"
+
+if [[ -z "$1" ]]; then
+    read -rp "URL: " url
+fi
+
+if [[ -z "$2" ]]; then
+    read -rp "File Format: " file_format
+fi
+
+if [[ "$file_format" != "mp4" ]]; then
+    args=$args"-x --audio-format $file_format"
+fi
+
+yt-dlp "$args" "$url" 
