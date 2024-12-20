29fd9a383c57a62435bccdfdae2e392b17a48d6c
Wapic <17051068+Wapic@users.noreply.github.com>
2024-12-20 17:39:03 +0100

    sync install between laptop and desktop

--- scripts/desktop-install.sh
+++ scripts/desktop-install.sh
@@ -11,25 +11,35 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
 # Setup SDDM
 yay -S sddm qt6-5compat qt6-declarative qt6-svg
 sudo git clone https://github.com/kamack38/sddm-greenleaf-theme.git /usr/share/sddm/themes/greenleaf
 sudo rm /usr/share/sddm/themes/greenleaf/background.png
-sudo mv /home/wapic/dotfiles/.config/hypr/wallpaper.png /usr/share/sddm/themes/greenleaf/background.png
-sed -i 's/Font=\"JetBrainsMono Nerd Font Mono\"/FiraCode Nerd Font Mono/' /usr/share/sddm/themes/greenleaf/theme.conf
+sudo mv /home/wapic/.config/hypr/wallpaper.png /usr/share/sddm/themes/greenleaf/background.png
+sudo sed -i 's/Font=\"JetBrainsMono Nerd Font Mono\"/FiraCode Nerd Font Mono/' /usr/share/sddm/themes/greenleaf/theme.conf
 echo -e "[Theme]\nCurrent=greenleaf" | sudo tee /etc/sddm.conf
-sudo systemctl enable sddm
 
 yay -S jdk11-openjdk jdk21-openjdk jdk8-openjdk libqalculate openssh wl-clipboard slurp grim \
-       hyprland hyprpaper hyprpicker hypridle xdg-desktop-portal-hyprland xdg-desktop-portal-gtk lxqt-policykit dunst wofi waybar gnome-keyring \
+       hyprland hyprpaper hyprpicker hypridle xdg-desktop-portal-hyprland xdg-desktop-portal-gtk lxqt-policykit dunst wofi waybar gnome-keyring clipboard-sync \
        pipewire pipewire-jack pipewire-pulse pipewire-alsa wireplumber pavucontrol audacity playerctl \
        kitty fzf bat zsh killall zip unzip tmux brightnessctl yt-dlp btop fastfetch \
        firefox chromium thunderbird nautilus nextcloud-client imv seahorse nwg-look mpv intellij-idea-community-edition chromium pinta stremio nodejs discord-canary \
        icedtea-web filezilla virt-viewer \
        mangohud vulkan-tools corectrl steam prismlauncher gamescope \
        gnome-theme-extras gtk-engine-murrine breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd ttf-hack-nerd qt5-wayland qt6-wayland
+
+# Hyprland 0.45.1-b generates the path as '/usr//usr/bin/Hyprland' instead of '/usr/bin/Hyprland'
+# in future releases this step will be unneccesary as it's getting replaced by uwsm
+if [ -f /usr/lib/systemd/user/hyprland-session.service ]; then
+    sudo sed -i 's/\/usr\/\//\//g' /usr/lib/systemd/user/hyprland-session.service
+fi
+
+# Enable systemd units
+systemctl enable --user --now gcr-ssh-agent.socket # Gnome-keyring ssh-agent
+systemctl enable --user --now clipboard-sync # Syncing clipboards between Wayland and XWayland
+sudo systemctl enable sddm # Display manager
 
 # Set keyboard layout
 sudo localectl set-x11-keymap "se" "pc104" "" "caps:swapescape"
@@ -44,3 +54,5 @@ rm -rf ~/Lavanda-gtk-theme
 sudo pacman -S neovim npm nodejs python3 pyright lua-language-server
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sudo npm install -g typescript-language-server bash-language-server typescript neovim
+
+echo 'Installation complete! reboot and your system will be done!'
