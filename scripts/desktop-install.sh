4595b9e35df6866bd072a8871ab6ca7ce385958c
Wapic <17051068+Wapic@users.noreply.github.com>
2024-12-21 21:04:24 +0100

    remove legacy fix

--- scripts/desktop-install.sh
+++ scripts/desktop-install.sh
@@ -22,19 +22,13 @@ sudo sed -i 's/Font=\"JetBrainsMono Nerd Font Mono\"/FiraCode Nerd Font Mono/' /
 echo -e "[Theme]\nCurrent=greenleaf" | sudo tee /etc/sddm.conf
 
 yay -S jdk11-openjdk jdk21-openjdk jdk8-openjdk libqalculate openssh wl-clipboard slurp grim \
-       hyprland hyprpaper hyprpicker hypridle xdg-desktop-portal-hyprland xdg-desktop-portal-gtk lxqt-policykit dunst wofi waybar gnome-keyring clipboard-sync \
+       hyprland hyprpaper hyprpicker hypridle xdg-desktop-portal-hyprland xdg-desktop-portal-gtk lxqt-policykit uwsm dunst wofi waybar gnome-keyring clipboard-sync \
        pipewire pipewire-jack pipewire-pulse pipewire-alsa wireplumber pavucontrol audacity playerctl \
        kitty fzf bat zsh zip unzip tmux brightnessctl yt-dlp btop fastfetch \
        firefox chromium thunderbird nautilus nextcloud-client imv seahorse nwg-look mpv intellij-idea-community-edition chromium pinta stremio nodejs discord-canary \
        icedtea-web filezilla virt-viewer \
        mangohud vulkan-tools corectrl steam prismlauncher gamescope \
        gnome-theme-extras gtk-engine-murrine breeze-icons noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-firacode-nerd ttf-hack-nerd qt5-wayland qt6-wayland
-
-# Hyprland 0.45.1-b generates the path as '/usr//usr/bin/Hyprland' instead of '/usr/bin/Hyprland'
-# in future releases this step will be unneccesary as it's getting replaced by uwsm
-if [ -f /usr/lib/systemd/user/hyprland-session.service ]; then
-    sudo sed -i 's/\/usr\/\//\//g' /usr/lib/systemd/user/hyprland-session.service
-fi
 
 # Enable systemd units
 systemctl enable --user --now gcr-ssh-agent.socket # Gnome-keyring ssh-agent
